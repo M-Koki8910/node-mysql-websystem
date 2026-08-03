4a04562e215b0ad47d41d6ed42dcd52656534d5a
M-Koki8910 <j2337@matsue-ct.ac.jp>
2026-08-03 00:35:48 +0000

    適切なユーザIDに基づいてToDoタスクを追加、表示しよう

--- config/passport.js
+++ config/passport.js
@@ -12,10 +12,10 @@ module.exports = function (app) {
         done(null, user.id);
     });
 
-    passport.deserializeUser(function (id, done) {
+    passport.deserializeUser(async function (id, done) {
         console.log("deserializeUser");
         try {
-            const user = User.findById(id);
+            const user = await User.findById(id);
             done(null, user);
         } catch (error) {
             done(error, null);
